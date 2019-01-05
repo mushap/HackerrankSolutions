e0a13bf940d1654bc63eda6f015b43081416d520
mushap kırım <mushapkirim@outlook.com>
2019-01-06 00:23:52 +0300

    Counting Valley problem is solved

---
+++ Algorithm/CountingValleys.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class CountingValleys
+    {
+        public int countingValleys(int n, string s)
+        {
+
+            int Ucounter = 0, Dcounter = 0, Sum = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'U')
+                    Ucounter++;
+                else
+                    Dcounter++;
+
+                if (Ucounter == Dcounter && s[i]== 'U')
+                    Sum++;
+            }
+
+            return Sum;
+
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -11,10 +11,14 @@ namespace Algorithm
         static void Main(string[] args)
         {
 
-           // SockMerchant sockMerchant = new SockMerchant();
-           // int[] arr = new int[]{ 10, 20, 20, 10, 10, 30, 50, 10, 20 };
-           // int[] arr1 = new int[]{ 1, 1, 3, 1, 2, 1, 3, 3, 3, 3 };
-           //int abc = sockMerchant.sockMerchant(arr1.Count(),arr1);
+            // SockMerchant sockMerchant = new SockMerchant();
+            // int[] arr = new int[]{ 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+            // int[] arr1 = new int[]{ 1, 1, 3, 1, 2, 1, 3, 3, 3, 3 };
+            //int abc = sockMerchant.sockMerchant(arr1.Count(),arr1);
+
+            //CountingValleys countingValleys = new CountingValleys();
+            //string s = "DDUUDDUDUUUD";
+            //countingValleys.countingValleys(s.Length,s);
 
             Console.ReadLine();
 
